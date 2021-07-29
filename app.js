7c2009287c660328a3fa1931b8322079336fb70d
elaiken <82184910+elaiken@users.noreply.github.com>
2021-07-28 21:16:43 -0400

    JavaScriptBpM2

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 
 window.addEventListener("load", (_) => {
-    const countText= document.getElementsByClassName('bpm__count')[0]
     const tapButton = document.getElementsByClassName('bpm__tap')[0]
-
-    console.log(tapButton,countText)
     
-    tapButton.addEventListener('click',(_) => {
-        console.log("clicked")
-        countText.innerHTML = 55
-    })
-
+    // console.log(tapButton,countText)
+    
+    
+    tapButton.addEventListener('click',(_) => collectTap())
+    
 }) 
-   
+var d1 = new Date();
+var n1 = d1.getTime();
+
+const bpmDigits = [n1]
+
+const collectTap = () => {
+    var d = new Date();
+    var n = d.getTime();
+    bpmDigits.push(n)
+    // console.log(bpmDigits)
+    
+    const countText= document.getElementsByClassName('bpm__count')[0]
+
+    const firstBeat = bpmDigits[0]
+    const lastIndex = bpmDigits.length - 1
+    const lastBeat = bpmDigits[lastIndex]
+
+    // console.log(lastBeat - firstBeat)
+    
+    const bpm = calculateBpm(bpmDigits.length, lastBeat - firstBeat)
+     
+    countText.innerHTML = bpm
+    
+    
+}
+
+const calculateBpm = (beatCount, timeDiff) => {
+
+    const bpm = 60000*(beatCount/timeDiff)
+    return (Math.round(bpm))
+}
+
+
